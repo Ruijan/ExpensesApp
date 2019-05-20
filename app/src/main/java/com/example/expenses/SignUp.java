a640e0f5285ae783d1693211d626f8d7bf95f2e2
Julien RECHENMANN <jrechenmann@ncelrnd2469.nce.amadeus.net>
2019-05-20 13:38:07 +0200

    create httprequest

--- app/src/main/java/com/example/expenses/SignUp.java
+++ app/src/main/java/com/example/expenses/SignUp.java
@@ -1,13 +1,27 @@
 package com.example.expenses;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import java.io.DataOutputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.Request;
+import com.android.volley.VolleyError;
+import com.android.volley.Response;
 
 public class SignUp extends AppCompatActivity {
 
@@ -23,9 +37,66 @@ public class SignUp extends AppCompatActivity {
         signupButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                EditText email = ((EditText)findViewById(R.id.email));
-                Snackbar.make(view, email.getText(), Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                //try {
+
+// Instantiate the RequestQueue.
+                    RequestQueue queue = Volley.newRequestQueue(view.getContext());
+                    String url ="http://heroku.com/BackEnd/index.php";
+
+// Request a string response from the provided URL.
+                    StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                            new Response.Listener<String>() {
+                                @Override
+                                public void onResponse(String response) {
+                                    // Display the first 500 characters of the response string.
+                                    System.out.println("Response is: "+ response.substring(0,500));
+                                }
+                            }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            System.out.println("That didn't work!");
+                        }
+                    });
+
+// Add the request to the RequestQueue.
+                    queue.add(stringRequest);
+
+                    /*URL url = new URL("http://example.com");
+                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                    con.setRequestMethod("POST");
+                    EditText email = ((EditText) findViewById(R.id.email));
+                    Snackbar.make(view, email.getText(), Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                    Map<String, String> parameters = new HashMap<>();
+                    parameters.put("param1", "val");
+
+                    con.setDoOutput(true);
+                    DataOutputStream out = new DataOutputStream(con.getOutputStream());
+                    String urlParameters = ParametersStringBuilder.getParamsString(parameters);
+                    out.writeBytes(urlParameters);
+                    out.flush();
+                    out.close();
+                    int responseCode = con.getResponseCode();
+                    System.out.println("\nSending 'POST' request to URL : " + url);
+                    System.out.println("Post parameters : " + urlParameters);
+                    System.out.println("Response Code : " + responseCode);
+
+                    BufferedReader in = new BufferedReader(
+                            new InputStreamReader(con.getInputStream()));
+                    String inputLine;
+                    StringBuffer response = new StringBuffer();
+
+                    while ((inputLine = in.readLine()) != null) {
+                        response.append(inputLine);
+                    }
+                    in.close();
+
+                    //print result
+                    System.out.println(response.toString());*/
+                /*}
+                catch(IOException e){
+                    e.printStackTrace();
+                }*/
             }
         });
     }
